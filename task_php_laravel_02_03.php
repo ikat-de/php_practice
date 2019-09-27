81acb2b4544a7f5ae9e43ca11478f6c302cd6665
Ikeda Atsushi <a.ikd05@hotmail.co.jp>
2019-09-27 05:16:25 +0000

    :sparkles: 課題提出 「PHP/Laravel 02 PHPの型と基本的な式を理解しよう」

---
+++ task_php_laravel_02_03.php
@@ -0,0 +1,10 @@
+<?php
+/* 課題3
+    $hello　という変数に　”Hello, “を $name という変数にあなたの名前を、　
+    $world という変数に　”‘s World!” を代入して、各変数を連結させて
+    “Hello, あなたの名前’s World!” と表示してみましょう。 */
+
+$hello = "Hello, ";
+$name = "Ikeda Atsushi";
+$world = "‘s World!";
+echo $hello . $name . $world;
